ecee34f24e74c230110288e1566818c016c86cab
Sam Michel <sam@toodlepip.co.uk>
2020-07-19 00:19:41 +0100

    Update wp_ga_tracking.js
    Added additional file extensions to check for download links

--- wp_ga_tracking.js
+++ wp_ga_tracking.js
@@ -1,3 +1,4 @@
+<script>
 jQuery(document).ready(function($) {
   let ga_event = function(href) {
       typeof(gtag) === "function" && gtag('event', 'download', { 'event_category': 'Downloads', 'event_label': href });
@@ -16,7 +17,9 @@ jQuery(document).ready(function($) {
   }
   $('.report_download a, a.report_download').click(function() { do_track($(this)); return false; });
   $('a[href]').each(function() {
-    $(this).attr('href').match(/\.(pdf|docx?|ppt|ppx|pps)$/i) && 
+    $(this).attr('href').match(/\.(pdf|docx?|pptx?|ppx|pps|xlsx?|zip|gz)$/i) && 
       $(this).click(function() { do_track($(this)); });
+      return false;
   });
 })
+</script>
